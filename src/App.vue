540874ad5694c10f38b7d1c7cefa97cf9873584e
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-16 02:07:27 +0500

    Images Added In the Gallery

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@
     <!-- Sidebar -->
     <SideBar />
     <VMain>
-      <!-- Cards -->
+      <ImageGallery />
     </VMain>
   </VApp>
 </template>
 <script>
 import SideBar from "./components/SideBar.vue";
+import ImageGallery from "./components/ImageGallery.vue";
 export default {
   components: {
     SideBar,
+    ImageGallery,
   },
 };
 </script>
---
+++ src/components/ImageGallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <VRow>
+    <VCol v-for="n in 205" :key="n" cols="2">
+      <VCard>
+        <VImg
+          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+          aspect-ratio="1"
+          cover
+        >
+          <template v-slot:placeholder></template>
+        </VImg>
+      </VCard>
+    </VCol>
+  </VRow>
+</template>
