afaa230f595241ffafda1594927aeded3bd46cdf
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-16 01:23:45 +0500

    Building a Webpage with Vuetify

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <VApp>
+    <!-- Sidebar -->
+    <SideBar />
+    <VMain>
+      <!-- Cards -->
+    </VMain>
+  </VApp>
+</template>
+<script>
+import SideBar from "./components/SideBar.vue";
+export default {
+  components: {
+    SideBar,
+  },
+};
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <VNavigationDrawer expand-on-hover rail>
+    <VList>
+      <VListItem
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        title="Elissa Perry"
+        subtitle="EPerry123@gmailcom"
+      ></VListItem>
+    </VList>
+    <VDivider />
+    <VList density="compact" nav>
+      <VListItem prepend-icon="mdi-image" title="Color Gallery" />
+    </VList>
+  </VNavigationDrawer>
+</template>
